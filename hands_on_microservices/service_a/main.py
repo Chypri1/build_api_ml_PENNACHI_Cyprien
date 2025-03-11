f24913430aa09beea985c5eb055f737aa24777bb
Cyprien Pennachi <cyprienpennachi@gmail.com>
2025-03-11 16:36:34 +0100

    ajout cicd docker

--- hands_on_microservices/service_a/main.py
+++ hands_on_microservices/service_a/main.py
@@ -2,14 +2,39 @@ from fastapi import FastAPI
 import httpx
 
 app = FastAPI()
-@app.get("/call-service-b")
-async def call_service_b():
+@app.get("/get_data")
+async def get_data():
     # Define the path to the Unix Domain Socket
     uds_path = "/tmp/service_b.sock"
     # Use a custom transport to make requests via the UDS
     transport = httpx.AsyncHTTPTransport(uds=uds_path)
     # Standard URL format without http+unix://
-    url = "http://service_b/data"
+    url = "http://service_b/get_data"
     async with httpx.AsyncClient(transport=transport) as client:
         response = await client.get(url)
+    return response.json()
+
+
+@app.post("/add_data")
+async def add_data():
+    # Define the path to the Unix Domain Socket
+    uds_path = "/tmp/service_b.sock"
+    # Use a custom transport to make requests via the UDS
+    transport = httpx.AsyncHTTPTransport(uds=uds_path)
+    # Standard URL format without http+unix://
+    url = "http://service_b/add_data"
+    async with httpx.AsyncClient(transport=transport) as client:
+        response = await client.post(url)
+    return response.json()
+
+@app.delete("/delete_data")
+async def add_data():
+    # Define the path to the Unix Domain Socket
+    uds_path = "/tmp/service_b.sock"
+    # Use a custom transport to make requests via the UDS
+    transport = httpx.AsyncHTTPTransport(uds=uds_path)
+    # Standard URL format without http+unix://
+    url = "http://service_b/delete_data"
+    async with httpx.AsyncClient(transport=transport) as client:
+        response = await client.delete(url)
     return response.json()
--- hands_on_microservices/service_b/main.py
+++ hands_on_microservices/service_b/main.py
@@ -14,3 +14,25 @@ async def get_data():
     print("format response :  " +str(response))
     return response
 
+@app.post("/add_data")
+async def add_data(documents_to_insert):
+    
+    client = MongoClient("mongodb://root:example@mongo:27017/")
+    db = client["local"]
+    collection = db["startup_log"]
+    result = collection.insert_many(documents_to_insert)
+    response = list(result)
+    print("format response :  " +str(response))
+    return response
+
+
+@app.post("/add_data")
+async def delete_data(id:str):
+    
+    client = MongoClient("mongodb://root:example@mongo:27017/")
+    db = client["local"]
+    collection = db["startup_log"]
+    result = collection.pop({id:id})
+    response = list(result)
+    print("format response :  " +str(response))
+    return response
